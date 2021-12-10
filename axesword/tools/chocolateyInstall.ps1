c3994dda3bf392e53eeae7684c6747108a7c7a93
Bram Duvigneau <bram@bramd.nl>
2021-12-10 22:06:36 +0100

    Add package for axesword

---
+++ axesword/tools/chocolateyInstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'axesword'
+
+$fileType      = 'exe'
+$url           = 'https://www.axes4.com/_Resources/Persistent/0/b/2/b/0b2b984658591aaf49c73985b3b1d57915114c82/axesWord.Setup.exe'
+$checksum      = 'BBBAA1F60DB288C2881516B65D8087CC39DB0C07FBAD56D45EDFF056F7A04AEB'
+
+$pp = Get-PackageParameters
+$params=@()
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = $fileType
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = '/quiet /noreboot' + ($params -join ' ')
+  validExitCodes = @(0)
+  softwareName   = "axesWord"
+}
+Install-ChocolateyPackage @packageArgs
+
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageName
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+Write-Host "$packageName installed to '$installLocation'"
+
+Register-Application "$installLocation\axesPDF-for-Word.dll"
+Write-Host "$packageName registered as $packageName"
---
+++ axesword/update.ps1
@@ -0,0 +1,35 @@
+import-module au
+. $PSScriptRoot\..\_scripts\all.ps1
+
+$releases    = 'https://www.axes4.com/en/products-services/axesword/download'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*[$]packageName\s*=\s*)('.*')"= "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*[$]fileType\s*=\s*)('.*')"   = "`$1'$($Latest.FileType)'"
+            "(?i)(^\s*[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url32)'"
+            "(?i)(^\s*[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+        }
+
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+file:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(checksum:).*"        = "`${1} $($Latest.Checksum32)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
+function global:au_GetLatest {
+     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
+     $regex   = '.exe$'
+     $url     = $download_page.links | ? href -match $regex | select -First 1 -expand href #2
+     $version = $download_page.Content.Split("Current Version: ")[1].split("</")[0]
+     return @{ Version = $version; URL32 = $url }
+}
+
+update
